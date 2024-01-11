f6b25a494281e91ff6693ada02b3e7f3a45890f6
ecommerceartisan <148409490+ecommerceartisan@users.noreply.github.com>
2024-01-11 12:16:22 +0800

    es
    es

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
@@ -19,6 +20,9 @@ connectDB();
 //rest object
 const app = express();
 
+//es
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //middlewares
 app.use(cors());
